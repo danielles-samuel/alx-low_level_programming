385a709cdf9dea4052bd755877aff2c09dc95407
danielles-samuel <chisomannastesia0@gmail.com>
2022-09-10 12:01:04 -0700

    first commit on variables_if_else_while project in c-programming updated. added a blank space after the declaration

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 /**
- * main - Prints all possible combination of two different digitd, in ascending  * order
+ * main - Prints all possible combinations of two different digits,
+ *  in ascending order
  *
  * Return: always 0.
  */
 int main(void)
 {
 	int digit1, digit2;
+
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (digit2 = digit1 < 9; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-			putchar(',');
-			putchar(' ');
-		}
+	for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+	{
+	putchar((digit1 % 10) + '0');
+	putchar((digit2 % 10) + '0');
+	if (digit1 == 8 && digit2 == 9)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
