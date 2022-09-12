81a35b9e90bb2ee973f4d93736d11356daa6ece7
danielles-samuel <chisomannastesia0@gmail.com>
2022-09-12 09:35:51 -0700

    updated

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,30 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Prints all possible combinations of three different digits
+ * main - main block
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
  *
- * Return: always 0
+ * Return: return 0
  */
 int main(void)
 {
-	int digit1, digit2, digit3;
+	int c;
+	int d;
+	int e = 0;
 
-	for (digit1 = 0; digit1 < 8; digit1++)
+	while (e < 10)
 	{
-	for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-	{
-	for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-	{
-	putchar((digit1 % 10) + '0');
-	putchar((digit2 % 10) + '0');
-	putchar((digit3 % 10) + '0');
-	if (digit1 < 7 || digit2 < 8 || digit3 < 9)
-	putchar(',');
-	putchar(' ');
-	}
-	}
+		d = 0;
+		while (d < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				if (c != d && d != e && e < d && d < c)
+				{
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
+
+					if (c + d + e != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+
+				c++;
+
+			}
+			d++;
+
+		}
+		e++;
+
 	}
 	putchar('\n');
 	return (0);
-}
 
+}
 
