f671b77499f390a684861eb1a022d1fa38d4f99e
danielles-samuel <chisomannastesia0@gmail.com>
2022-09-16 03:34:59 -0700

    first commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,15 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * _isupper - checks for uppercase character
- * @c: Variable text
- * Return: Always 0
+ * main - Test function for positive and negative
+ * Return: 0
  */
-int _isupper(int c)
+int main(void)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,12 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_numbers(void);
+#include <stdio.h>
 
-#endif
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+void print_times_table(int n);
+
+#endif /*Main_H */
