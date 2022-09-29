4af1e9d0eda0ba4b25f86dabb2a87854f8e004e2
danielles-samuel <chisomannastesia0@gmail.com>
2022-09-28 18:18:35 -0700

    Addnew

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,53 +1,47 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * length - helps do it
+ * _strlen_recursion - returns the length of a string.
  * @s: string
- * Return: string length
+ * Return: the length of a string.
  */
-int length(char *s)
-{
-	int i = 0;
 
-	if (*s)
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	return (0);
+	else
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * comparator - compares each character of the string.
+ * @s: string
+ * @n1: smallest iterator.
+ * @n2: biggest iterator.
+ * Return: .
+ */
+
+int comparator(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
 	{
-		i = i + length(s + 1);
-		return (i += 1);
+	if (n1 == n2 || n1 == n2 + 1)
+	return (1);
+	return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
 }
 
 /**
- * helper2 - helps more
- * @i: integer i
- * @s: string
- * Return: int value
+ * is_palindrome - detects if a string is a palindrome.
+ * @s: string.
+ * Return: 1 if s is a palindrome, 0 if not.
  */
-int helper2(int i, char *s)
-{
-	if (*s)
-	{
-		if (*s != s[length(s) - i])
-		{
-			return (0);
-		}
-		else
-		{
-			return (helper2(i + 1, s + 1));
-		}
-	}
 
-	return (1);
-}
-
-/**
- * is_palindrome - is it paldindrome
- * @s: string to check
- * Return: boolean
- */
 int is_palindrome(char *s)
 {
-	int i = 1;
-
-	return (helper2(i, s));
+	if (*s == '\0')
+	return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,17 +1,1 @@
-#This project covers C - Recursion
-
-###The purpose of this project is to:
-		
-	1. General
-
-#####What is recursion
-#####How to implement recursion
-#####In what situations you should implement recursion
-#####In what situations you shouldn’t implement recursion
-
-
-   ###Technologies Scripts written in Bash 4.3.11(1) C files are compiled using gcc (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0 Copyright (C) 2019 Free Software Foundation, Inc. This is free software; see the source for copying conditions. There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Tested on Ubuntu 22.04 LTS
-
-
-
-
+#Recursion
